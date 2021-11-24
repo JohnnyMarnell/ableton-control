b85832f22a45f2a29e3a64fe1adb65cb1ed44203
Johnny Marnell <jmarnell@spotify.com>
2021-11-24 08:33:24 -0500

    Minor cleanup

--- AbletonPythonResticleResource.py
+++ AbletonPythonResticleResource.py
@@ -1,22 +1,20 @@
-
-""" Just a few routes as examples """
 class AbletonPythonResticleResource:
+    """ Implementing a few routes as examples """
     def __init__(self, application, song):
         self.song = song
         self.application = application
 
     def add_routes(self, server):
         ok = {'ok': True}
-        server.add_route('/tempo', lambda p, h: {'tempo': self.song.tempo}, 'GET')
+        server.add_route('GET', '/tempo', lambda p, h: {'tempo': self.song.tempo}, 'GET')
 
-        server.add_route('/tempo/{bpm}', lambda p, h: self.set_tempo(float(p['bpm'])), 'POST')
+        server.add_route('POST', '/tempo/{bpm}', lambda p, h: self.set_tempo(float(p['bpm'])))
 
-        server.add_route('/play', lambda p, h: self.song.start_playing() or ok, 'POST')
-        server.add_route('/stop', lambda p, h: self.song.stop_playing() or ok, 'POST')
+        server.add_route('POST', '/play', lambda p, h: self.song.start_playing() or ok)
+        server.add_route('POST', '/stop', lambda p, h: self.song.stop_playing() or ok)
 
     def set_tempo(self, tempo):
         self.song.tempo = tempo
         return {'tempo': tempo}
-
 
     # etc, etc
--- SimpleResticleServer.py
+++ SimpleResticleServer.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 import sys, socket, collections, re, json, time
 
-Route = collections.namedtuple('Route', 'pattern handler method')
+Route = collections.namedtuple('Route', 'method pattern handler')
 
 
 class SimpleResticleServer:
@@ -19,12 +19,13 @@ class SimpleResticleServer:
         print('Listening on port %s ...' % port)
 
     """ Leverage RegEx named groups for quick and dirty REST path parameters """
-    def add_route(self, path, handler, method='GET'):
+    def add_route(self, method, path, handler):
         pattern = re.compile(r'/\{([^}]+)\}').sub(r'/(?P<\1>[^/]+)', path)
-        self.routes.append(Route(re.compile(pattern), handler, method))
+        self.routes.append(Route(method, re.compile(pattern), handler))
         print('Added route:', method, path)
 
-    def handle_request(self, request):
+    """ Parse HTTP request (path, headers, etc), find route and dispatch """
+    def serve_request(self, request):
         lines = request.split('\n')
         method, path, scheme = lines[0].split(' ')
         headers = {}
@@ -34,27 +35,33 @@ class SimpleResticleServer:
             colon = header.index(":")
             headers[header[0:colon].strip().lower()] = header[colon + 1:].strip()
         # todo: if json header, parse body
+        # todo: query string params
         try:
             for route in self.routes:
                 if route.method == method:
                     match = route.pattern.match(path)
                     if match:
                         body = route.handler(match.groupdict(), headers)
-                        return 'HTTP/1.0 200 OK\n\n' + json.dumps(body)
+                        return 'HTTP/1.0 200 OK\nContent-Type: application/json\n\n' + json.dumps(body)
             return 'HTTP/1.0 404 NOT FOUND\n\nNot Found'
         except ValueError as e:
             print('Server error', e)
             return 'HTTP/1.0 500 INTERNAL SERVER ERROR\n\nInternal Server Error'
 
+    """ Handle any pending requests / connections """
     def tick(self):
-        try:
-            client_connection, client_address = self.server_socket.accept()
-            request = client_connection.recv(2048).decode()
-        except:
-            return
-        response = self.handle_request(request)
-        client_connection.sendall(response.encode())
-        client_connection.close()
+        while True:
+            try:
+                client, client_address = self.server_socket.accept()
+                request = client.recv(2048).decode()
+            except socket.error as e:
+                if e.errno == socket.errno.EAGAIN:
+                    break
+                else:
+                    raise e
+            response = self.serve_request(request)
+            client.sendall(response.encode())
+            client.close()
 
     def shutdown(self):
         self.server_socket.close()
@@ -68,8 +75,8 @@ if __name__ == "__main__":
         server.shutdown()
         sys.exit(0)
     signal.signal(signal.SIGINT, signal_handler)
-    server.add_route('/echo/{a}/{b}', lambda path_params, headers: {
-        'path_params': path_params, 'headers': headers}, 'GET')
+    server.add_route('GET', '/echo/{a}/{b}', lambda path_params, headers: {
+        'path_params': path_params, 'headers': headers})
     while True:
         server.tick()
         time.sleep(0.200)
